746e5336379baaef2a5bca16382660c964c1bc4c
SeemaMistry <seema.m.1996@gmail.com>
2023-04-07 19:07:12 -0400

    feat: add new url path to products/

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,4 +3,5 @@ from products import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/', views.product, name='product'),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("This is the home page")
+
+def product(request):
+    return HttpResponse("This is the products page")
