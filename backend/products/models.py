0c194dd9251f42822a692bb9b53b8f35d0770b14
SeemaMistry <seema.m.1996@gmail.com>
2023-04-07 21:34:32 -0400

    feat: add str def to Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=30)
     desc = models.TextField()
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.name
