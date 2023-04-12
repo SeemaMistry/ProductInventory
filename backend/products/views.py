56a0d02e7464aa0ba20e5e8de9a68f5d36ddd357
SeemaMistry <seema.m.1996@gmail.com>
2023-04-12 12:00:25 -0400

    feat: add DDL conditional alert-success upon successful form submission

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def home(request):
+    context = {'success': False}
     # handle post requests
     if request.method == "POST":
         # handle the form
         name = request.POST['name']
         desc = request.POST['desc']
-        print(name, desc)
+        product = Product(name=name, desc=desc)
+        context = {'success': True}
+        product.save()
 
-    return render (request, 'index.html')
+    return render (request, 'index.html', context)
 
 def product(request):
     return render (request, 'products.html')
