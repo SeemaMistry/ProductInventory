b5562bfd23ae051298ee4715fd875c38a23cc123
SeemaMistry <seema.m.1996@gmail.com>
2023-04-12 11:40:28 -0400

    feat: handle form post request and save form content to database

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
+    # handle post requests
+    if request.method == "POST":
+        # handle the form
+        name = request.POST['name']
+        desc = request.POST['desc']
+        print(name, desc)
+
     return render (request, 'index.html')
 
 def product(request):
