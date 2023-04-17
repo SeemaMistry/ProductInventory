6a013a5a01105932957841c396013bff4a50c9d5
SeemaMistry <seema.m.1996@gmail.com>
2023-04-17 16:03:59 -0400

    feat: create Inventory model with OneToOne relation to Product

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+class Inventory(models.Model):
+    # product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE,)
+    count = models.IntegerField(default=0)
+    location = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.product.name + " " + str(self.count)
